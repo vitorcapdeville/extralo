36af71c5ccdf587a813d44d8e02f8abb1c66ed5d
vitorcapdeville <vgcapdeville@hotmail.com>
2024-07-22 08:55:35 -0300

    ✨Adicionar logging nas chamadas do ETL.

--- src/extralo/etl.py
+++ src/extralo/etl.py
@@ -1,27 +1,31 @@
+import logging
+
 import pandas as pd
 
 from extralo.destinations import Destination
 from extralo.sources import Source
-from logging import Logger
 
 
 class ETL:
-    def __init__(self, source: Source, destination: Destination, logger: Logger) -> None:
+    def __init__(self, source: Source, destination: Destination) -> None:
         self._source = source
         self._destination = destination
-        self._logger = logger
+        self._logger = logging.getLogger("etl")
 
     def execute(self):
         data = self.extract()
-        self._logger.info(f"Extracted {len(data)} records")
         data = self.transform(data)
         self.load(data)
 
     def extract(self) -> pd.DataFrame:
-        return self._source.extract()
+        data = self._source.extract()
+        self._logger.info(f"Extracted {len(data)} records from {self._source.__class__.__name__}")
+        return data
 
     def transform(self, data) -> pd.DataFrame:
+        self._logger.info("No transformation made.")
         return data
 
     def load(self, data) -> pd.DataFrame:
+        self._logger.info(f"Loaded {len(data)} records into {self._destination.__class__.__name__}")
         return self._destination.load(data)
