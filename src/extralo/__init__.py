90da3549933bdd25d9066267aa85d9a62079f587
Vitor Capdeville <vgcapdeville@hotmail.com>
2024-11-09 13:53:39 +0000

    🚑️ incluir repr para Spark source e destination (consertando ultimo commit)

--- src/extralo/__init__.py
+++ src/extralo/__init__.py
@@ -30,7 +30,7 @@ __all__ = [
     "XLSXDestination",
     "SQLAppendDestination",
     "DeltaLakeDestination",
-    "SparkDeltaLakeDestination",
     "DeltaLakeSource",
+    "SparkDeltaLakeDestination",
     "SparkDeltaLakeSource",
 ]
